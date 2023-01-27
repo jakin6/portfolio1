44a409464b2f182fb0658921dc1bf61bedcbf5c0
jakin6 <irijakin@gmail.com>
2023-01-27 10:05:48 +0100

    initial commit

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Intro.css'
+const Intro = () => {
+  return (
+    <div className="intro">
+        <div className="i-left">
+            <div className="i-name">
+               <span> Hy! I Am</span>
+               <span> IRISA Jakin</span>
+               <span>Frontend Developer with high level 
+                of experience in web designing and development,
+                 producting 
+                the Quality work</span>
+            </div>
+            <button className="button i-button">
+                Hire me
+            </button>
+        </div>
+        <div className="i-right">
+            I am right position
+        </div>
+    </div>
+  )
+}
+
+export default Intro
